265ff8970ac30af4011de7c003c7b6bcbc13a445
MohammedVaraliya <varaliyamohammed1@gmail.com>
2022-09-22 00:14:35 +0530

    Add Data Structure "Singly Linked List"

--- 04_singly_linked_list.py
+++ 04_singly_linked_list.py
@@ -111,6 +111,40 @@ class SinglyLinkedList:
             return 0
         return 1 + self.len_recursive(node.next)
 
+    def swap_nodes(self, key_1, key_2):
+        
+        if key_1 == key_2:
+            return
+
+        prev_1 = None
+        curr_1 = self.head
+        while curr_1 and curr_1.data != key_1:
+            prev_1 = curr_1
+            curr_1 = curr_1.next
+
+        prev_2 = None
+        curr_2 = self.head
+        while curr_2 and curr_2.data != key_2:
+            prev_2 = curr_2
+            curr_2 = curr_2.next
+
+        if not curr_1 and curr_2:
+            print("No Nodes found")
+            return
+
+        if prev_1:
+            prev_1.next = curr_2
+        else:
+            self.head = curr_2
+
+        if prev_2:
+            prev_2.next = curr_1
+        else:
+            self.head = curr_1
+
+        curr_1.next, curr_2.next = curr_2.next, curr_1.next
+
+
 
 
 llist = SinglyLinkedList()
@@ -131,3 +165,16 @@ llist.delete_node_at_pos(1)
 llist.print_list()
 
 print(llist.len_recursive(llist.head))
+
+print("\n\n")
+
+swappingNode = SinglyLinkedList()
+swappingNode.append("A")
+swappingNode.append("B")
+swappingNode.append("C")
+swappingNode.append("D")
+
+swappingNode.swap_nodes("A", "D")
+
+swappingNode.print_list()
+
