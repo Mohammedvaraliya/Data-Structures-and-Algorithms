f5651c03c5fa50c9dd672fc02a0fc9e1c90fa4cc
MohammedVaraliya <varaliyamohammed1@gmail.com>
2022-10-14 16:05:27 +0530

    Add DS "SinglyLL -- Merge Two Sorted Lists"

---
+++ 05_singlyLL_merge.py
@@ -0,0 +1,103 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        cur_node = self.head
+        self.head = new_node
+        new_node.next = cur_node
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def merge_sorted(self, llist):
+        
+        p = self.head
+        q = llist.head
+        s = None
+
+        if not p:
+            return q
+        if not q:
+            return p
+
+        if p and q:
+            if p.data <= q.data:
+                s = p
+                p = s.next
+            else:
+                s = q
+                q = s.next
+            new_head = s
+
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+
+        if not p:
+            s.next = q
+        if not q:
+            s.next = p
+        
+        return new_head
+                
+
+
+
+
+
+llist_1 = SinglyLinkedList()
+llist_2 = SinglyLinkedList()
+
+llist_1.append(1)
+llist_1.append(5)
+llist_1.append(7)
+llist_1.append(8)
+llist_1.append(10)
+
+llist_2.append(2)
+llist_2.append(3)
+llist_2.append(4)
+llist_2.append(6)
+llist_2.append(9)
+
+llist_1.print_list()
+print("\n")
+llist_2.print_list()
+print("\n")
+
+llist_1.merge_sorted(llist_2)
+llist_1.print_list()
--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 2. Determine if parenthesis are balanced
 3. Convert integer to binary
 4. Singly Liked List
-5. Reverse String
+5. Singly Linked Lists -- Merge Two Sorted Lists
+6. Reverse String
