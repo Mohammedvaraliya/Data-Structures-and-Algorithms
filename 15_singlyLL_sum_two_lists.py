8907406ba5d4cd36a5e6cfcc92a1937be60683f6
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-20 11:59:56 +0530

    Add Data Structure "SinglyLL -- Sum Two Lists"

--- 15_singlyLL_sum_two_lists.py
+++ 15_singlyLL_sum_two_lists.py
@@ -1,5 +1,11 @@
 '''
-Singly Linked Lists -- Move Tail to Head
+Singly Linked Lists -- Sum Two Lists
+
+llist 1 : 3 6 5 and
+
+llist 2 : 2 4 8
+
+365 + 248 = 613
 '''
 
 
@@ -42,15 +48,55 @@ class SinglyLinkedList:
             print(cur_node.data)
             cur_node = cur_node.next
 
+    def sum_two_lists(self, llist):
+        
+        p = self.head
+        q = llist.head
+
+        sum_llist = SinglyLinkedList()
+
+        carry = 0
+        while p or q:
+            if not p:
+                i = 0
+            else:
+                i = p.data
+            if not q:
+                j = 0
+            else:
+                j = q.data
+            s = i + j + carry
+            # print("S :" + str(s))
+            if s>= 10:
+                carry = 1
+                remainder = s % 10
+                sum_llist.prepend(remainder)
+            else:
+                carry = 0
+                sum_llist.prepend(s)
+            if p:
+                p = p.next
+            if q:
+                q = q.next
+        sum_llist.print_list()
+
 
     
-llist = SinglyLinkedList()
-llist.append("A")
-llist.append("B")
-llist.append("C")
-llist.append("D")
-llist.append("E")
-llist.append("F")
+llist1 = SinglyLinkedList()
+llist1.append(5)
+llist1.append(6)
+llist1.append(3)
 
-llist.print_list()
+llist1.print_list()
 print("\n")
+
+llist2 = SinglyLinkedList()
+llist2.append(8)
+llist2.append(4)
+llist2.append(2)
+
+llist2.print_list()
+print("\n")
+
+print(365 + 248)
+llist1.sum_two_lists(llist2)
