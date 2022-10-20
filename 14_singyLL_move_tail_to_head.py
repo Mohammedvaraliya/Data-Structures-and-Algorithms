0b08c87df4d36ae7b1570973c6de06bd0005ebed
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-20 11:06:02 +0530

    Add Data Structure "SinglyLL Move Tail to Head"

---
+++ 14_singyLL_move_tail_to_head.py
@@ -0,0 +1,72 @@
+'''
+Singly Linked Lists -- Move Tail to Head
+'''
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        cur_node = self.head
+        self.head = new_node
+        new_node.next = cur_node
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def move_tail_to_head(self):
+
+        last = self.head
+        second_to_last = None
+        
+        while last.next:
+            second_to_last = last
+            last = last.next
+            
+        last.next = self.head
+        second_to_last.next = None
+        self.head = last
+
+
+    
+llist = SinglyLinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+llist.append("E")
+llist.append("F")
+
+llist.print_list()
+print("\n")
+
+llist.move_tail_to_head()
+llist.print_list()
--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Data-Structure
 
-1. The Stack data Structure
-2. Determine if parenthesis are balanced
-3. Convert integer to binary
-4. Singly Liked List
-5. Singly Linked Lists -- Length of List
-6. Singly Linked Lists -- Node Swap
-7. Singly Linked Lists -- Reverse
-8. Singly Linked Lists -- Merge Two Sorted Lists
-9. Singly Linked Lists -- Remove Duplicates
+01. The Stack data Structure
+02. Determine if parenthesis are balanced
+03. Convert integer to binary
+04. Singly Liked List
+05. Singly Linked Lists -- Length of List
+06. Singly Linked Lists -- Node Swap
+07. Singly Linked Lists -- Reverse
+08. Singly Linked Lists -- Merge Two Sorted Lists
+09. Singly Linked Lists -- Remove Duplicates
 10. Singly Linked Lists -- Nth-to-Last Node
 11. Singly Linked Lists -- Count Occurrences
 12. Singly Linked Lists -- Rotate
 13. Singly Linked Lists -- Is Palindrome
+14. Singly Linked Lists -- Move Tail to Head
 16. Reverse String
