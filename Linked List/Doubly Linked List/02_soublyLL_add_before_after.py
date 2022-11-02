04391b56819843c460e9b05ea45e78f36bc18048
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-11-02 12:22:56 +0530

    Add Data Structure "DoublyLLAdd Node Before/After"

--- Linked List/Doubly Linked List/02_soublyLL_add_before_after.py
+++ Linked List/Doubly Linked List/02_soublyLL_add_before_after.py
@@ -41,18 +41,60 @@ class DoublyLinkedList:
             print(cur.data)
             cur = cur.next
 
-    def add_after_node(self, key):
-        pass
+    def add_after_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.next is None and cur.data == key:
+                self.append(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                nxt = cur.next
+                cur.next = new_node
+                new_node.next = nxt
+                new_node.prev = cur
+                nxt.prev = new_node
+            cur = cur.next
+                
 
-    def add_before_node(self, key):
-        pass
+    def add_before_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.prev is None and cur.data == key:
+                self.prepend(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                prev = cur.prev
+                prev.next = new_node
+                cur.prev = new_node
+                new_node.next = cur
+                new_node.prev = prev
+            cur = cur.next
     
 
 
-dllist = DoublyLinkedList()
-dllist.append(1)
-dllist.append(2)
-dllist.append(3)
-dllist.append(4)
+dllist1 = DoublyLinkedList()
+dllist1.append(1)
+dllist1.append(2)
+dllist1.append(3)
+dllist1.append(4)
 
-dllist.print_list()
+dllist1.add_after_node(1, 11)
+dllist1.add_after_node(2, 12)
+dllist1.add_after_node(4, 14)
+
+dllist1.print_list()
+print("\n")
+
+dllist2 = DoublyLinkedList()
+dllist2.append(1)
+dllist2.append(2)
+dllist2.append(3)
+dllist2.append(4)
+
+dllist2.add_before_node(1, 11)
+dllist2.add_before_node(2, 12)
+dllist2.add_before_node(4, 14)
+
+dllist2.print_list()
