99ed7aeeb6ebce505a64b57946e089ec50547479
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-08 15:23:10 +0530

    Add Data Structure "Arrays Advance Game"

---
+++ Arrays/07_array_advance_game.py
@@ -0,0 +1,27 @@
+'''
+"array advance game"
+
+you are given an array of non-negative integers. For example:
+ 
+ [3,3,1,0,2,0,1].
+
+Each number represents the maximum you can advance in the array. 
+
+Question:
+Is it possible to advance from the start of the array to the last element?
+'''
+
+def array_advance(array):
+    furtest_reached = 0
+    last_idx = len(array) - 1
+    i = 0
+    while i <= furtest_reached and furtest_reached < last_idx:
+        furtest_reached = max(furtest_reached, array[i] + i)
+        i += 1
+
+    return furtest_reached >= last_idx
+
+
+if __name__ == "__main__":
+    A = [3, 3, 1, 0, 2, 0 , 1]
+    print(array_advance(A))
