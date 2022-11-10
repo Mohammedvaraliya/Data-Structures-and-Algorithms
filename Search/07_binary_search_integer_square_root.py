107a4347f9e408e2b5a9da55ea3948a87950e746
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-10 15:01:45 +0530

    Add DSA "Binary Search: Integer Square Root"

---
+++ Search/07_binary_search_integer_square_root.py
@@ -0,0 +1,15 @@
+'''
+write a function that takes a non-negative integer and returns the largest integer whose square is less than or equal to the integer given:
+
+Example:
+
+    Assume input is integer 300.
+        
+    Then the expected output of the function should be 17 since 17 squared is 289 which is strictly less than 300. Note that 18 squared is 324 which is strictly greater than 300, so the number 17 is the correct response.
+'''
+
+k = 300
+ 
+def integer_square_root(number):
+    pass
+
