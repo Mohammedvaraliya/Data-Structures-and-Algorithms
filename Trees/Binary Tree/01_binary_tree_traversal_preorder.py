cb12ec37fa41a81d9fc35d10545697aed248d426
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-15 19:16:41 +0530

    Add DSA 'Binary Trees: Traversal Algorithms'

--- Trees/Binary Tree/01_binary_tree_traversal_preorder.py
+++ Trees/Binary Tree/01_binary_tree_traversal_preorder.py
@@ -28,13 +28,15 @@ class BinaryTree(object):
 
     
 '''
-    1-2-4-5-3-6-7-
+    Pre-Order Traversal
+    
                     1
                 /       \
                2          3
              /   \       /  \  
             4      5    6     7
 
+    1-2-4-5-3-6-7-
 '''
 
         
