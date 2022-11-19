82893a003c9cecc06f124edf4bb5f03b1eedc672
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-19 22:09:16 +0530

    Add DSA 'Binary Search Trees: Checking the BST Property'

--- Trees/Binary Search Tree/03_binary_search_tree_checking.py
+++ Trees/Binary Search Tree/03_binary_search_tree_checking.py
@@ -61,7 +61,7 @@ class BinarySearchTree:
                 return self._is_bst_satisfied(cur_node.right, cur_node.right.data)
             else:
                 return False
-                
+
         return True
 
 
