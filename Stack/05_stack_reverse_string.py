d04e279a43129fe40720518e7b0a55708ad06fa2
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-24 15:00:07 +0530

    Add The Stack data Structure : Reverse String

--- Stack/05_stack_reverse_string.py
+++ Stack/05_stack_reverse_string.py
@@ -19,3 +19,24 @@ class Stack:
     def size(self):
         return len(self.container)
 
+def reverse_string(string):
+    stack = Stack()
+
+    for char in string:
+        stack.push(char)
+
+    str = ''
+    while stack.size() != 0:
+        str += stack.pop()
+
+    return str
+
+
+
+if __name__ == "__main__":
+
+    str1 = "You can do it."
+    str2 = "ofCourse"
+
+    print(reverse_string(str1))
+    print(reverse_string(str2))
