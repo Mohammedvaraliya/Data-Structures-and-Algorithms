3aad144ce529e8fe728c758e3caac9a4c9aa8f5f
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-29 22:15:12 +0530

    Add Graph Data Structure

--- Graph/01_graph_data_structure.py
+++ Graph/01_graph_data_structure.py
@@ -1,3 +1,33 @@
+class Graph:
+    def __init__(self, edges):
+        self.edges = edges
+        self.graph_dict = {}
+
+        for start, end in self.edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start] = [end]
+        
+        print("Graph Dictionary : ", self.graph_dict)
+
+    def get_paths(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return [path]
+
+        if start not in self.graph_dict:
+            return []
+
+        paths = []
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_paths = self.get_paths(node, end, path)
+                for p in new_paths:
+                    paths.append(p)
+
+        return paths
 
 
 
@@ -13,5 +43,11 @@ if __name__ == "__main__":
         ("Paris", "Dubai"),
         ("Paris", "New York"),
         ("Dubai", "New York"),
-        ("New York", "Toronto"),
+        ("New York", "Toronto")
     ]
+
+    route_graph = Graph(routes)
+
+    start = "Mumbai"
+    end = "New York"
+    print(route_graph.get_paths(start, end))
--- Graph/README.md
+++ Graph/README.md
@@ -4,6 +4,29 @@
 ### 01. Graph Data Structure
 
     Graph Data Structure
-    ...
-
     
+
+    Example Graph:
+
+                  />  Paris
+                 /     |     \  
+                /      |      \
+               /       |       \
+              /        |        \> New York       
+        Mumbai         |         />         \ 
+              \        |        /            \
+               \       |       /              \
+                \      |      /                \
+                 \     |>    /                  \> Toronto
+                  \>  Dubai   
+
+
+    The Routes:
+
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto")
+                         
