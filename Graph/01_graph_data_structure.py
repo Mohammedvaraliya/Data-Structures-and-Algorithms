e9bae4f87e90514ee61241c75f64eb7d06070484
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-30 17:50:41 +0530

    Add Graph Data Structure

--- Graph/01_graph_data_structure.py
+++ Graph/01_graph_data_structure.py
@@ -29,6 +29,27 @@ class Graph:
 
         return paths
 
+    def get_shortest_path(self, start, end, path=[]):
+
+        path = path + [start]
+
+        if start == end:
+            return path
+
+        if start not in self.graph_dict:
+            return None
+
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if sp:
+                    if shortest_path is None or len(sp) < len(shortest_path):
+                        shortest_path = sp
+
+        return shortest_path
+
+
 
 
 
@@ -47,7 +68,20 @@ if __name__ == "__main__":
     ]
 
     route_graph = Graph(routes)
+    print("\n")
 
     start = "Mumbai"
     end = "New York"
-    print(route_graph.get_paths(start, end))
+    print(f"Paths between {start} and {end} : ", route_graph.get_paths(start, end))
+    print("\n")
+
+    print(f"Shortest path between {start} and {end} : ", route_graph.get_shortest_path(start, end))
+    print("\n")
+
+    start = "Paris"
+    end = "New York"
+    print(f"Paths between {start} and {end} : ", route_graph.get_paths(start, end))
+    print("\n")
+
+    print(f"Shortest path between {start} and {end} : ", route_graph.get_shortest_path(start, end))
+    print("\n")
