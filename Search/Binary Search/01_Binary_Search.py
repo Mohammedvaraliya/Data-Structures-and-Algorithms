cdf872e2045643ffb6268878b8f0032fa0b3a4a6
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-01 18:54:39 +0530

    Add Data Structure Binary Search : find element

--- Search/Binary Search/01_Binary_Search.py
+++ Search/Binary Search/01_Binary_Search.py
@@ -1,3 +1,16 @@
+from calculate_time_decorator import time_it
+
+@time_it
+# Linear Search
+def linear_search(data, target):
+    for i in range(len(data)):
+        if data[i] == target:
+            print(f"At index Number :",i)
+            return True
+
+    return False
+
+@time_it
 # Iterative Binary Search
 def binary_search_iterative(data, target):
     low = 0
@@ -32,7 +45,7 @@ def binary_search_recursive(data, target, low, high):
 if __name__ == "__main__":
 
     data = [2,4,5,6,7,8,55,65,76,86,90,89,90,111,209,879]
-    target = 4
+    target = 209
 
     low = 0
     high = len(data) - 1
@@ -40,6 +53,28 @@ if __name__ == "__main__":
     # Finding the mid point
     mid = (low + high) // 2
     print(data[mid])
+    print("\n")
+
+    print(linear_search(data, target))
+    print("\n")
+
+    print(binary_search_iterative(data, target))
+    print(binary_search_recursive(data, target, 0, len(data) - 1))
+    print("~"*20, "end", "~"*20)
+    print("\n")
+
+    data = [i for i in range(100002)]
+    target = 100001
+
+    low = 0
+    high = len(data) - 1
+
+    # Finding the mid point
+    mid = (low + high) // 2
+    print(data[mid])
+    print("\n")
+
+    print(linear_search(data, target))
     print("\n")
 
     print(binary_search_iterative(data, target))
