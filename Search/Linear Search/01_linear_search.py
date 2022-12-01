ed56ebf776b9fdca3c9d8888fa684222c0e31915
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-01 14:34:26 +0530

    Add Data Structure Linear Search

---
+++ Search/Linear Search/01_linear_search.py
@@ -0,0 +1,14 @@
+def linear_search(data, target):
+    for i in range(len(data)):
+        if data[i] == target:
+            return True
+
+    return False
+
+
+
+if __name__ == "__main__":
+    data = [2,4,5,6,7,8,55,65,76,86,90,89,90,111,209,879]
+    target = 4
+
+    print(linear_search(data, target))
