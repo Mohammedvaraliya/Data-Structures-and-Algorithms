a3318cb0ff721df1c35ae72fd69858d161fe1f02
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-01 14:44:40 +0530

    Add Data Structure Linear Search

--- Search/Linear Search/01_linear_search.py
+++ Search/Linear Search/01_linear_search.py
@@ -1,6 +1,7 @@
 def linear_search(data, target):
     for i in range(len(data)):
         if data[i] == target:
+            print(f"At index Number :",i)
             return True
 
     return False
@@ -9,6 +10,6 @@ def linear_search(data, target):
 
 if __name__ == "__main__":
     data = [2,4,5,6,7,8,55,65,76,86,90,89,90,111,209,879]
-    target = 4
+    target = 879
 
     print(linear_search(data, target))
---
+++ Search/Linear Search/README.md
@@ -0,0 +1,8 @@
+# Data-Structures And Algorithms
+
+
+### 01. Linear search
+
+    Linear Search
+    Linear search is a very simple search algorithm. 
+    In this type of search, a sequential search is made over all items one by one.
