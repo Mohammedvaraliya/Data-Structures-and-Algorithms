56f14e6c326750c150a5024a7f36c7ba1ed15a0a
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-12-03 13:22:18 +0530

    Add Data Structure Sorting Algorithm : Bubble Sort

--- Sort/Bubble Sort/01_bubble_sort.py
+++ Sort/Bubble Sort/01_bubble_sort.py
@@ -2,13 +2,15 @@ def bubble_sort(elements):
     size = len(elements)
 
     for i in range(size - 1):
-        for j in range(size - 1):
+        swapped = False
+        for j in range(size - 1 - i):
             if elements[j] > elements[j+1]:
                 tmp = elements[j]
                 elements[j] = elements[j+1]
                 elements[j+1] = tmp
-
-
+                swapped = True
+        if not swapped:
+            break
 
 
 
@@ -16,6 +18,8 @@ def bubble_sort(elements):
 if __name__ == "__main__":
     
     elements = [5, 9, 2, 1, 67, 34, 88, 34]
+    # elements = [1, 3, 5, 7]
+    # elements = ["varaliya", "mohammed", "sahil", "sameer", "pranjal", "subhashish"] # can perform with string also
 
     bubble_sort(elements)
     print(elements)
