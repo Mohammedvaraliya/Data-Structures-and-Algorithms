16953f2776feca9eb24295e1f5dbed3e01f801e8
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-07-11 17:51:08 +0530

    Update Maximum Depth of Binary Tree

--- Trees/Binary Tree/09_binary_tree_maximum_depth.py
+++ Trees/Binary Tree/09_binary_tree_maximum_depth.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Node(object):
     def __init__(self, value):
         self.value = value
@@ -9,8 +11,10 @@ class BinaryTree(object):
         self.root = Node(root)
 
     def print_max_dept(self, traversal_type):
-        if traversal_type == "maximum _depth":
+        if traversal_type == "maximum_depth":
             return self.maxDepth(self.root)
+        elif traversal_type == "maximum_depth_BFS":
+            return self.maxDepth_BFS(self.root)
         else:
             print("Depth type " + str(traversal_type) + " is not supported.")
             return False
@@ -20,6 +24,23 @@ class BinaryTree(object):
             return 0
         
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+    def maxDepth_BFS(self, root):
+        if not root:
+            return 0
+        
+        level = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            level += 1
+        return level
 
 
 
@@ -33,13 +54,13 @@ if __name__ == "__main__":
     tree1.root.right.left = Node(15)
     tree1.root.right.right = Node(7)
 
-    X = tree1.print_max_dept("maximum _depth")
+    X = tree1.print_max_dept("maximum_depth_BFS")
     print(X)
 
     tree2 = BinaryTree(1)
     tree2.root.right = Node(2)
 
-    Y = tree2.print_max_dept("maximum _depth")
+    Y = tree2.print_max_dept("maximum_depth")
     print(Y)
 
     
