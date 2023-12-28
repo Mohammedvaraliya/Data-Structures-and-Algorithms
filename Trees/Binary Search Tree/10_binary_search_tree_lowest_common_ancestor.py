44a6b860ec9a6afcd08ac0f280aaa7f8e1e58e4b
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-12-28 22:17:28 +0530

    Added Binary Search Tree problem "Lowest Common Ancestor of a Binary Search Tree"

--- Trees/Binary Search Tree/10_binary_search_tree_lowest_common_ancestor.py
+++ Trees/Binary Search Tree/10_binary_search_tree_lowest_common_ancestor.py
@@ -1,50 +1,72 @@
-class BinarySearchTreeNode:
-    def __init__(self, data):
-        self.data = data
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
         self.left = None
         self.right = None
 
-    # Problem Logic
-    def lowestCommonAncestor(self, root, p , q):
+class BinarySearchTreeNode:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         cur = root
         
         while cur:
-            if p.data > cur.data and q.data > cur.data:
+            if p.val > cur.val and q.val > cur.val:
                 cur = cur.right
-            elif p.data < cur.data and q.data < cur.data:
+            elif p.val < cur.val and q.val < cur.val:
                 cur = cur.left
             else:
-                return cur.data
+                return cur
+
+# Helper function to build a BST from a list
+def buildBST(nums):
+    if not nums:
+        return None
+
+    root = TreeNode(nums[0])
+    for num in nums[1:]:
+        if num is not None:
+            root = insert(root, num)
+
+    return root
+
+# Helper function to insert a value into a BST
+def insert(root, val):
+    if not root:
+        return TreeNode(val)
+    
+    if val < root.val:
+        root.left = insert(root.left, val)
+    else:
+        root.right = insert(root.right, val)
+    
+    return root
+
 
 
 
 
 if __name__ == "__main__":
+    solution = BinarySearchTreeNode()
 
-    BST1 = BinarySearchTreeNode(6)
-    BST1.left = BinarySearchTreeNode(2)
-    BST1.right = BinarySearchTreeNode(8)
-    BST1.left.left = BinarySearchTreeNode(0)
-    BST1.left.right = BinarySearchTreeNode(4)
-    BST1.left.right.left = BinarySearchTreeNode(3)
-    BST1.left.right.right = BinarySearchTreeNode(5)
-    BST1.right.left = BinarySearchTreeNode(7)
-    BST1.right.right = BinarySearchTreeNode(9)
+    nums_1 = [6, 2, 8, 0, 4, 7, 9, None, None, 3, 5]
+    p_val_1, q_val_1 = 2, 8
+    root1 = buildBST(nums_1)
+    p1 = TreeNode(p_val_1)
+    q1 = TreeNode(q_val_1)
+    result1 = solution.lowestCommonAncestor(root1, p1, q1)
+    print(result1.val)
 
-    p1 = BST1.left
-    q1 = BST1.right
-    print(BST1.lowestCommonAncestor(BST1, p1, q1))
-    
-    p2 = BST1.left
-    q2 = BST1.left.right
-    print(BST1.lowestCommonAncestor(BST1, p2, q2))
+    nums_2 = [6, 2, 8, 0, 4, 7, 9, None, None, 3, 5]
+    p_val_2, q_val_2 = 2, 4
+    root2 = buildBST(nums_2)
+    p2 = TreeNode(p_val_2)
+    q2 = TreeNode(q_val_2)
+    result2 = solution.lowestCommonAncestor(root2, p2, q2)
+    print(result2.val)
 
-    BST2 = BinarySearchTreeNode(2)
-    BST2.left = BinarySearchTreeNode(1)
-    
-    p3 = BST2
-    q3 = BST2.left
-    print(BST2.lowestCommonAncestor(BST2, p3, q3))
-
-
-    
+    nums_3 = [2, 1]
+    p_val_3, q_val_3 = 2, 1
+    root3 = buildBST(nums_3)
+    p3 = TreeNode(p_val_3)
+    q3 = TreeNode(q_val_3)
+    result3 = solution.lowestCommonAncestor(root3, p3, q3)
+    print(result3.val)
