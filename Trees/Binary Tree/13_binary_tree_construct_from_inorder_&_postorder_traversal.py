2ab15d9a830041520ff943a66e93b3fb0df00b86
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2024-01-16 19:22:41 +0530

    Added Binary Tree problem "Construct Binary Tree from Inorder and Postorder Traversal"

--- Trees/Binary Tree/13_binary_tree_construct_from_inorder_&_postorder_traversal.py
+++ Trees/Binary Tree/13_binary_tree_construct_from_inorder_&_postorder_traversal.py
@@ -6,15 +6,37 @@ class TreeNode:
 
 class ConstructBinaryTree:
 
-    def buildTree(self, postorder: list[int], inorder: list[int]) -> 'TreeNode':
+    # Time Complexity O(n^2)
+    def buildTree1(self, inorder: list[int], postorder: list[int]) -> 'TreeNode':
         if not postorder or not inorder:
             return None
         
-        root = TreeNode(postorder[0])
-        mid = inorder.index(postorder[0])
-        root.left = self.buildTree(postorder[1:mid + 1], inorder[:mid])
-        root.right = self.buildTree(postorder[mid + 1:], inorder[mid + 1:])
+        root = TreeNode(postorder.pop())
+        idx = inorder.index(root.val)
+        root.right = self.buildTree1(inorder[idx + 1:], postorder)
+        root.left = self.buildTree1(inorder[:idx], postorder)
         return root
+
+    # Time Complexity O(n)
+    def buildTree2(self, inorder: list[int], postorder: list[int]) -> 'TreeNode':
+
+        def buildTreeHelper(left, right):
+            if left > right:
+                return None
+
+            rootVal = postorder.pop()
+            rootNode = TreeNode(rootVal)
+
+            idx = inorderIndexMap[rootVal]
+            rootNode.right = buildTreeHelper(idx + 1, right)
+            rootNode.left = buildTreeHelper(left, idx - 1)
+            return rootNode
+
+        inorderIndexMap = {}
+        for (i, val) in enumerate(inorder):
+            inorderIndexMap[val] = i
+
+        return buildTreeHelper(0, len(postorder) - 1)
     
 
 def print_tree(root):
@@ -46,12 +68,12 @@ if __name__ == "__main__":
 
     postorder1 = [9,15,7,20,3]
     inorder1 = [9,3,15,20,7]
-    root1 = builder.buildTree(postorder1, inorder1)
+    root1 = builder.buildTree2(inorder1, postorder1)
     result1 = print_tree(root1)
     print(result1)
 
     postorder2 = [-1]
     inorder2 = [-1]
-    root2 = builder.buildTree(postorder2, inorder2)
+    root2 = builder.buildTree1(inorder2, postorder2)
     result2 = print_tree(root2)
     print(result2)
