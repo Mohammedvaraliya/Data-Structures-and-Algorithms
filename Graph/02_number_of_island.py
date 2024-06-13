5204d8d61575e91d91a7f39e2773ea84c30f6163
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2024-06-13 12:26:28 +0530

    Added Graph Datastructure "NUMBER OF ISLANDS - Leetcode 200"

--- Graph/02_number_of_island.py
+++ Graph/02_number_of_island.py
@@ -7,21 +7,33 @@ class Solution:
         
         rows, cols = len(grid), len(grid[0])
         visit = set()
-        island = 0
+        islands = 0
 
         def bfs(row, col):
             q = collections.deque()
-            visit.add(row, col)
+            visit.add((row, col))
             q.append((row, col))
 
             while q:
                 row, col = q.popleft()
+                directions = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+
+                for dirRow, dirCol in directions:
+                    r, c = row + dirRow, col + dirCol
+                    if ((r) in range(rows) and
+                        (c) in range(cols) and
+                        grid[r][c] == "1" and
+                        (r, c) not in visit):
+                        q.append((r, c))
+                        visit.add((r, c))
 
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == "1" and (row, col) not in visit:
                     bfs(row=row, col=col)
-                    island += 1
+                    islands += 1
+
+        return islands
 
 
 
@@ -31,3 +43,27 @@ class Solution:
 if __name__ == "__main__":
 
     obj = Solution()
+    
+    grid1 = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+    ]
+    print(obj.numIslands(grid=grid1))
+
+    grid2 = [
+        ["1","1","0","0","0"],
+        ["1","1","0","0","0"],
+        ["0","0","1","0","0"],
+        ["0","0","0","1","1"]
+    ]
+    print(obj.numIslands(grid=grid2))
+
+    grid3 = [
+        ["1","1","1"],
+        ["0","1","0"],
+        ["1","1","1"]
+    ]
+    print(obj.numIslands(grid=grid3))
+
