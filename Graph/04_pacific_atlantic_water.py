cf273e6182310e2fb07a1a9c0422634efd445f87
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2024-06-21 12:16:46 +0530

    Added Data Structure problem Graph "Pacific Atlantic"

---
+++ Graph/04_pacific_atlantic_water.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pacificAtlantic(self, heights: list[list[int]]) -> list[list[int]]:
+        pass
+
+
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    
+    
+    
+    
+    
