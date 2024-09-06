22826e32d007135518b068b0145ec747c4bfd7d4
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2024-09-07 00:08:25 +0530

    Added Greedy Programming Problem "Jump Game"

--- Greedy/02_jump_index.py
+++ Greedy/02_jump_index.py
@@ -1,6 +1,13 @@
 class Solution:
     def canJump(self, nums: list[int]) -> bool:
-        pass
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        
+        return True if goal == 0 else False
+
 
 
 
