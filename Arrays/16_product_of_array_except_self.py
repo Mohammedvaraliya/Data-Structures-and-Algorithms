41063b98d225c485dc66121a54bb9b375a3aa9a1
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2025-07-04 15:08:14 +0530

    Refactor Product of Array Except Self into Solution class and enhance README with detailed explanation

--- Arrays/16_product_of_array_except_self.py
+++ Arrays/16_product_of_array_except_self.py
@@ -1,25 +1,27 @@
-def productExceptSelf(nums: list[int]) -> list[int]:
-    res = [1] * len(nums)
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        res = [1] * len(nums)
 
-    prefix = 1
-    for i in range(len(nums)):
-        res[i] = prefix
-        prefix *= nums[i]
-    postfix = 1
-    for i in range(len(nums) -1, -1, -1):
-        res[i] *= postfix
-        postfix *= nums[i]
-    return res
-
-
-
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
 
 
 
 if __name__ == "__main__":
 
-    X = productExceptSelf(nums = [1,2,3,4])
+    obj = Solution()
+
+    X = obj.productExceptSelf(nums = [1, 2, 3, 4])
     print(X)
 
-    Y = productExceptSelf(nums = [-1,1,0,-3,3])
+    Y = obj.productExceptSelf(nums = [-1, 1, 0, -3, 3])
     print(Y)
