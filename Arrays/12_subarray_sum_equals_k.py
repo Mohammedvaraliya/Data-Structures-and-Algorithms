7479c942e148437c01d18cecdd2dbb9530bd959f
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2025-07-10 23:23:39 +0530

    Add Subarray Sum problem statement, solution implementation, and update README

--- Arrays/12_subarray_sum_equals_k.py
+++ Arrays/12_subarray_sum_equals_k.py
@@ -1,33 +1,34 @@
-def subarraySum(nums, k: int) -> int:
+class Solution:
+    def subarraySum(self, nums, k: int) -> int:
+        sumdict = {0:1}
+        count = 0
+        s = 0
 
-    sumdict = {0:1}
-    n = len(nums)
-    count = 0
-    s = 0
+        for num in nums:
+            s += num
+            if s - k in sumdict:
+                count += sumdict[s - k]
+            if s in sumdict:
+                sumdict[s] += 1
+            else:
+                sumdict[s] = 1
 
-    for num in nums:
-        s += num
-        if s - k in sumdict:
-            count += sumdict[s - k]
-        if s in sumdict:
-            sumdict[s] += 1
-        else:
-            sumdict[s] = 1
-
-    return count
+        return count
 
 
 if __name__ == "__main__":
 
-    nums1 = [1, 1, 1]
-    nums2 = [1, 2, 3]
-    nums3 = [3, 4, 7, 2, -3, 1, 4, 2, 1]
+    obj = Solution()
 
-    X = subarraySum(nums1, k=2)
+
+    nums1 = [1, 1, 1]
+    X = obj.subarraySum(nums1, k=2)
     print(X) # Output: 2
 
-    Y = subarraySum(nums2, k=3)
+    nums2 = [1, 2, 3]
+    Y = obj.subarraySum(nums2, k=3)
     print(Y) # Output: 2
 
-    Z = subarraySum(nums3, k=7)
+    nums3 = [3, 4, 7, 2, -3, 1, 4, 2, 1]
+    Z = obj.subarraySum(nums3, k=7)
     print(Z) # output: 6
